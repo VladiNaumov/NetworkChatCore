da63561d33cde89ce48f9661385e525574ef00cc
Sim <https://github.com/VladiNaumov>
2022-07-12 00:24:04 +0300

    NetWorkChatCoreFx

---
+++ Client/src/main/java/com/naumdeveloper/Main.java
@@ -0,0 +1,24 @@
+package com.naumdeveloper;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/window.fxml"));
+        Parent root = fxmlLoader.load();
+        Controller controller = fxmlLoader.getController();
+        primaryStage.setTitle("March Chat");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setOnCloseRequest(event -> controller.exit());
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
